76a00a9443f5bee3e92568860dc000f0685a127b
Bauyrzhan03 <103853806+Bauyrzhan03@users.noreply.github.com>
2022-04-22 14:20:19 +0600

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-app.use("/index", require("./index"));
-app.use("/contact", require("./contact"));
-app.listen(port, () =>
-    console.log(`App listening at http://localhost:${port}`)
-);
