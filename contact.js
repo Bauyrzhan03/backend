d685b78c3f784e8d05f8c0216599c09c5ee8d533
Bauyrzhan03 <103853806+Bauyrzhan03@users.noreply.github.com>
2022-04-22 14:19:58 +0600

    Delete contact.js

--- contact.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-router
-    .route("/contact")
-    .get((req, res) => res.send("GET CONTACT"))
-    .post((req, res) => res.send("POST CONTACT"));
-module.exports = router;
