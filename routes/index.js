db90c95f7e7e1915f17070e9d7d196046acb5c06
b0zsz <96700102+b0zsz@users.noreply.github.com>
2022-05-04 15:56:35 +0600

    Delete routes directory

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-router
-    .route("/index")
-    .get((req, res) => res.send("/index.ejs"))
-    .post((req, res) => res.send("POST"));
-module.exports = router;
--- routes/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-app.set('views','ejs');
-app.use("/index", require("./index"));
-app.use("/contact", require("./contact"));
-app.listen(port, () =>
-    console.log(`App listening at http://localhost:${port}`)
-);
